003a9375b0db84c6d5a9140545844f91131e175f
unknown <devparvej19>
2023-01-28 21:08:31 +0600

    project section add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+// 'use strict'
+
+// var cursor = document.querySelector(".follow-img");
+
+// document.addEventListener("mousemove", (e) => {
+
+//     var x = e.clientX;
+//     var y = e.clientY;
+
+//     cursor.style.left = `${x}px`;
+//     cursor.style.top = `${y}px`;
+
+// })
+// let cursorImg = document.querySelectorAll(".follow-img");
+// for (let i = 0; i < cursorImg; i++) {
+//     cursorImg[i].addEventListener("mousemove", (e) => {
+//         let x = e.clientX;
+//         let y = e.clientY;
+//         this.style.left = `${x}px`;
+//         this.style.top = `${y}px`;
+//     })
+
+// }
+var cursor = document.querySelectorAll(".follow-img");
+document.addEventListener("mousemove", (e) => {
+    var x = e.clientX;
+    var y = e.clientY;
+    console.log(cursor.length)
+    for (let i = 0; i < cursor.length; i++) {
+        cursor[i].style.left = `${x}px`;
+        cursor[i].style.top = `${y}px`;
+    }
+})
+
+
 function openNav() {
     document.getElementById("myNav").style.display = "block";
 }
@@ -16,3 +51,5 @@ window.addEventListener(
     },
     false
 );
+
+
